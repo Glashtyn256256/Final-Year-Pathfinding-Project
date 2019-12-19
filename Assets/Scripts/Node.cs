a49e4d7d265c779c9bc55b16bdc65cb8f721287a
Benjamin Moore <recovery903@gmail.com>
2019-12-19 21:50:32 +0000

    Node class added

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Container of data with some limited functionality, not in heriting from monobehaviour could make pathfinding more fficient
+
+public enum NodeType
+{
+    Open = 0,
+    Blocked = 1
+}
+
+public class Node
+{
+    public NodeType nodeType = NodeType.Open;
+
+    //integers to track position in the 2d array.
+    public int xIndex = -1;
+    public int yIndex = -1;
+
+    //Keep track of node position
+    public Vector3 position;
+
+    //neighbour of the node
+    public List<Node> neighbours = new List<Node>(); 
+
+    public Node previous = null;
+
+    //Constructor
+    public Node(int xIndex, int yIndex, NodeType nodeType)
+    {
+        this.xIndex = xIndex;
+        this.yIndex = yIndex;
+        this.nodeType = nodeType;
+    }
+
+    public void Reset()
+    {
+        previous = null;
+    }
+}
