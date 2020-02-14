91db1f659e4aea2a16af9f4823098961c8b237d4
Recov <Recov@LAPTOP-5SU6BEGF>
2020-02-14 14:36:39 +0000

    Created a seperate flowfield scene which is the template for the other four. Main menu scene buttons now go to all the different scenes, created controller script for the main menu.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PathfindingMapOneOnClick()
+    {
+        SceneManager.LoadScene("PathfindingMapOne");
+    }
+
+    public void PathfindingMapTwoOnClick()
+    {
+        SceneManager.LoadScene("PathfindingMapTwo");
+    }
+
+    public void PathfindingMapThreeOnClick()
+    {
+        SceneManager.LoadScene("PathfindingMapThree");
+    }
+
+    public void PathfindingMapCustomOnClick()
+    {
+        SceneManager.LoadScene("PathfindingMapCustom");
+    }
+
+    public void MinHeapMapOneOnClick()
+    {
+        SceneManager.LoadScene("MinHeapMapOne");
+    }
+
+    public void MinHeapMapTwoOnClick()
+    {
+        SceneManager.LoadScene("MinHeapMapTwo");
+    }
+
+    public void MinHeapMapThreeOnClick()
+    {
+        SceneManager.LoadScene("MinHeapMapThree");
+    }
+
+    public void MinHeapMapCustomOnClick()
+    {
+        SceneManager.LoadScene("MinHeapMapCustom");
+    }
+
+    public void FlowfieldMapOneOnClick()
+    {
+        SceneManager.LoadScene("FlowfieldMapOne");
+    }
+
+    public void FlowfieldMapTwoOnClick()
+    {
+        SceneManager.LoadScene("FlowfieldMapTwo");
+    }
+
+    public void FlowfieldMapThreeOnClick()
+    {
+        SceneManager.LoadScene("FlowfieldMapThree");
+    }
+
+    public void FlowfieldMapCustomOnClick()
+    {
+        SceneManager.LoadScene("FlowfieldMapCustom");
+    }
+}
